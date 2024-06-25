8fb504f94810ec01932c561aada97ab77d8729e9
cqt <celexusercal@gmail.com>
2024-06-25 03:42:14 -0800

    Update savev2.lua

--- savev2.lua
+++ savev2.lua
@@ -12,7 +12,7 @@ if not CopyToClipboard then
 end
 
 local SaveManager = {} do
-	SaveManager.Folder = getgenv().settings_folder or "evelSettings"
+	SaveManager.Folder = getgenv().settings_folder or "Evel"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
